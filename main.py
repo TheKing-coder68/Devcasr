56669942256c2fdd95cffdd8dd1d24d8ef94c9ea
King <emokpairegenerous@gmail.com>
2021-05-28 11:49:23 -0400

    Finished the login/signup logic. Stoll have to do the CSS for the Login.html page.

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', None)
 def home():
 	return render_template("home.html")
 
-@app.route('/auth')
-def auth():
-	return render_template("auth.html")
+@app.route('/signup', methods=['POST','GET'])
+def signup():
 	if request.method=="POST":
 		user = mongo.db.User
 
@@ -23,12 +22,29 @@ def auth():
 		password = request.form.get("signup-password") 
 
 		if not re.search('^[^@ ]+@[^@ ]+\.[^@ .]{2,}$', email):
-        		flash("That is not a valid email entry, please try again.") 
+			flash("That is not a valid email entry, please try again.")
 
 		user.insert_one({"Username":user_name, "Password":password, "Email":email})
 		flash("You have signed in.")
-		return render_template("auth.html")
-	return render_template("auth.html")
+		return render_template("signup.html")
+	return render_template("signup.html")
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+	if request.method == "POST":
+		user = mongo.db.User
+
+		user_name= request.form.get("login-username")
+		password = request.form.get("login-password")
+
+		if not user.find_one({"Username":user_name, "Password":password}):
+			flash("Your login information is incorrect, please try again.")
+
+		if user.find_one({"Username":user_name, "Password":password}):
+			flash("You have successfully logged in!!")
+			return render_template("home.html")
+
+	return render_template("login.html")
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port='8080', debug=True)
