7f6dbb90b67d4f79279ad47c5f98c9e40ab29453
King <emokpairegenerous@gmail.com>
2021-06-10 10:13:38 -0400

    fixed a typo

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def projects():
 	project_name = posts.find({})
 	return render_template('projects.html', project_name = project_name)
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port='8080)
+	app.run()
